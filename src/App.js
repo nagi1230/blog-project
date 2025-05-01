f4167bb171f76c0ba28937c57e17b56b4513fc62
dilpavittar <dilpavittarpal@antiersolutions.com>
2025-05-01 16:40:16 +0530

    fix: add / route for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 
+// import React from 'react';
+// import AppRoutes from "./Routs/AppRoutes"
+// import Profile from './Pages/Profile/Profile';
+// import Portfolio from './Portfolio/Portfolio.jsx';
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Blog from './Portfolio/Blog.jsx';
+// const App = () => {
+//   return (
+//     <div>
+//       {/* <AppRoutes /> */}
+//       {/* <Profile /> */}
+//       <BrowserRouter basename="/blog-project">
+//         <Routes>
+//           {/* <Route path="/" element={<Portfolio />} /> */}
+//           <Route path="/blog" element={<Blog />} />
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
+// };
+// export default App;
+
+
 import React from 'react';
-import AppRoutes from "./Routs/AppRoutes"
-import Profile from './Pages/Profile/Profile';
-import Portfolio from './Portfolio/Portfolio.jsx';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Blog from './Portfolio/Blog.jsx';
+import Portfolio from './Portfolio/Portfolio.jsx';
+
 const App = () => {
   return (
-    <div>
-      {/* <AppRoutes /> */}
-      {/* <Profile /> */}
-      <BrowserRouter basename="/blog-project">
-        <Routes>
-          {/* <Route path="/" element={<Portfolio />} /> */}
-          <Route path="/blog" element={<Blog />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter basename="/blog-project">
+      <Routes>
+        {/* <Route path="/" element={<Portfolio />} /> */}
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
+
 export default App;
+
