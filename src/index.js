50c5bdbac686cd135bde2b8d4ba0e7a6ed08ae9d
dilpavittar <dilpavittarpal@antiersolutions.com>
2025-05-01 17:45:32 +0530

    test: force hello message

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import { Provider } from 'react-redux';
 import Store from './Store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={Store}>
-    <App />
-  </Provider>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
